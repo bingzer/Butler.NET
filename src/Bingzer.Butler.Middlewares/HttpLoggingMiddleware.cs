ba25513bd3a434b68f9f9b5acd693493b7c67cee
ricky@bingzer.com <ricky@bingzer.com>
2015-05-16 00:34:52 -0400

    Added LoggingMiddleware

---
+++ src/Bingzer.Butler.Middlewares/HttpLoggingMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.Logging;
+using System.Threading.Tasks;
+
+namespace Bingzer.Butler.Middlewares
+{
+    public class HttpLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public HttpLoggingMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+        {
+            _next = next;
+            _logger = loggerFactory.CreateLogger("HTTP");
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await _next(context);
+            _logger.LogInformation(context.Request.Method + " " + context.Response.StatusCode + " " + context.Request.Path);
+        }
+        
+    }
+}
---
+++ src/Bingzer.Butler.Middlewares/LoggingMiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Builder;
+
+namespace Bingzer.Butler.Middlewares
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseHttpLogging(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<HttpLoggingMiddleware>();
+        }
+    }
+}
